ef3983aa51a2b98d9b57890dac8c7a8eb849920d
Elden Williams <eldenw28@yahoo.com>
2022-04-21 16:53:54 -0700

    added logging?

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,6 +1,6 @@
-// export default () => new Response("Hello Netlify!");
+export default () => new Response("Hello Netlify!");
 
-export default () => {
-    console.log("Processing request for");
-    new Response("Hello Netlify!");
-}
+// export default () => {
+//    console.log("Processing request for");
+//   new Response("Hello Netlify!");
+//}
